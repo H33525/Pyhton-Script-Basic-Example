c69c3722e709fcf16d6aae8583bd2adb7ae958a2
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-09-08 18:39:02 +0530

    Add files via upload

---
+++ pythen script basic example.py
@@ -0,0 +1,18 @@
+import randome
+
+answer= random.randint(1,10)
+While True:
+
+try:
+guess= int(input('guess a number 1~10')
+if 0<guess<11:
+print("You are a genius")
+break
+
+else:
+  print('hey bozo, I said 1~10')
+
+expect value Error:
+
+print(please enter a number')
+continue
